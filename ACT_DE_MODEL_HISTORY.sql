f386e31f4f47f6e9db0aca2ede6379b1b79a7465
lianghuashuai <51349061+lianghuashuai@users.noreply.github.com>
2020-01-10 14:10:33 +0800

    2
    2

---
+++ ACT_DE_MODEL_HISTORY.sql
@@ -0,0 +1,7 @@
+prompt Importing table ACT_DE_MODEL_HISTORY...
+set feedback off
+set define off
+insert into ACT_DE_MODEL_HISTORY (ID, NAME, MODEL_KEY, DESCRIPTION, MODEL_COMMENT, CREATED, CREATED_BY, LAST_UPDATED, LAST_UPDATED_BY, REMOVAL_DATE, VERSION, MODEL_EDITOR_JSON, MODEL_ID, MODEL_TYPE)
+values ('5984cfc0-d2dc-4134-9779-ab396e8dc674', 'report', 'report', null, null, '30-3月 -19 11.17.50.148000 上午', 'admin', '30-3月 -19 11.18.01.604000 上午', 'admin', null, 1, '{"models":[{"id":"4789fd84-2724-4129-adc8-838df8ca2725","name":"report","version":1,"modelType":0,"description":null,"stencilSetId":null,"createdBy":"admin","lastUpdatedBy":"admin","lastUpdated":"2019-03-30T03:17:13.413+0000"}],"theme":"theme-1","icon":"glyphicon-asterisk"}', '0474b0b0-ead7-4e8a-8499-aa91b4cf1fdf', 3);
+
+prompt Done.
